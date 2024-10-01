3f08772081267747238f96e25e47734ed109dfe8
rockmel03 <kamalswag2@gmail.com>
2024-10-02 00:16:31 +0530

    responsive menu and footer component

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -3,25 +3,29 @@ import footerBg from "../../assets/Images/footer-bg.png";
 
 export const Footer = () => {
   return (
-    <footer className=" py-10">
+    <footer className="py-5 laptop:py-10">
       <Layout
-        className={`relative min-h-[60vh] flex items-center justify-center flex-col gap-5`}
+        className={`relative laptop:min-h-[60vh] flex items-center justify-center flex-col gap-5 p-10  `}
       >
-        <img src={footerBg} alt="" className="absolute" />
-        <h3 className="relative z-[2] text-white font-semibold text-[2em]">
+        <img
+          src={footerBg}
+          alt=""
+          className="absolute w-full h-full laptop:w-auto laptop:h-auto object-cover tablet:rounded-lg"
+        />
+        <h3 className="relative z-[2] text-white font-semibold text-[1.5em] laptop:text-[2em] text-center">
           Subscribe to get 50% discount price
         </h3>
-        <form className="relative z-[2] bg-white text-black p-2 rounded-full">
+        <form className="relative z-[2] bg-white text-black p-1 tablet:p-2 rounded-full flex items-center">
           <input
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
-            className="px-2 text-[.875em] font-medium outline-none bg-transparent text-inherit"
+            className="px-2 text-[.875em] font-medium outline-none bg-transparent text-inherit w-[70%]"
           />
           <button
             type="submit"
-            className="bg-secondary text-white px-5 py-2 rounded-full text-[.875em] font-medium"
+            className="bg-secondary text-white px-5 py-2 rounded-full text-[.875em] font-medium whitespace-nowrap"
           >
             Order now
           </button>
--- src/componets/Menu/Menu.jsx
+++ src/componets/Menu/Menu.jsx
@@ -6,13 +6,13 @@ export const Menu = () => {
   return (
     <div className="py-20">
       <Layout>
-        <h3 className="text-[2em] font-semibold">
+        <h3 className="text-[2em] font-semibold text-center laptop:text-left mb-10">
           Special menu{" "}
           <span className="inline-block border-b-4 border-primary leading-none">
             for you
           </span>
         </h3>
-        <section className="w-full flex flex-wrap items-center gap-3 justify-around px-10">
+        <section className="w-full flex flex-wrap items-center gap-3 justify-around laptop:px-10">
           {ProductData.length > 0 &&
             ProductData.map((item, idx) => (
               <Card key={idx} data={item} layout={2} />
