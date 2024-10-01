18510ce7e7ca45494b1f3c596476f0ace65f4f31
rockmel03 <kamalswag2@gmail.com>
2024-10-01 23:54:51 +0530

    make responsive about and sevice component

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -6,31 +6,32 @@ export const About = () => {
   return (
     <section
       style={{ backgroundImage: `url(${aboutBg})` }}
-      className="bg-cover bg-center mt-28"
+      className="bg-cover bg-center laptop:mt-28"
     >
-      <Layout className="flex items-center gap-5">
+      <Layout className="flex items-center gap-5 flex-col-reverse tablet:flex-row py-10 tablet:py-0">
         <div className="flex-1 flex items-center justify-center">
-          <div className="w-fit h-fit p-3 rounded-3xl bg-glass -translate-y-[10%]">
+          <div className="w-fit h-fit p-3 rounded-3xl bg-glass tablet:-translate-y-[10%]">
             <div className="rounded-md overflow-hidden">
               <img src={aboutCoffee} alt="about us" />
             </div>
           </div>
         </div>
-        <div className="flex-1 flex flex-col items-start gap-5">
+        <div className="flex-1 flex flex-col items-center tablet:items-start gap-5 text-center tablet:text-left">
           <h2 className="text-[2em] font-semibold">
             About{" "}
             <span className="inline-block border-b-4 border-primary leading-none">
               us
             </span>
           </h2>
-          <h3 className="font-semibold text-[1.5em]">
+          <h3 className="font-semibold text-[1.3em] laptop:text-[1.5em]">
             We provide quality coffee,
             <br /> and ready to deliver.
           </h3>
           <p className="text-light text-[1.125em]">
             We are a company that makes and distributes
-            <br /> delicious drinks. our main product is made with a <br />
-            secret recipe and available in stores worldwide.
+            <br className="hidden laptop:block" /> delicious drinks. our main
+            product is made with <br className="hidden laptop:block" />a secret
+            recipe and available in stores worldwide.
           </p>
           <button className="text-[.85em] px-4 py-2 rounded-full bg-secondary font-semibold text-[#F4AE26]">
             Get your coffee
--- src/componets/DeliveryService/DeliveryService.jsx
+++ src/componets/DeliveryService/DeliveryService.jsx
@@ -3,15 +3,15 @@ import { deliveryServiceData } from "../../Constants";
 
 export const DeliveryService = () => {
   return (
-    <section>
+    <section className="py-20 laptop:py-5">
       <Layout>
-        <h3 className="text-[2em] font-semibold">
+        <h3 className="text-[2em] font-semibold text-center tablet:text-left">
           How to use delivery{" "}
           <span className="inline-block border-b-4 border-primary leading-none">
             service
           </span>
         </h3>
-        <div className="flex items-center justify-around py-20">
+        <div className="flex items-center justify-around py-20 flex-col laptop:flex-row gap-10">
           {deliveryServiceData.map((item, idx) => (
             <div key={idx} className="flex flex-col gap-2 items-center ">
               <img
